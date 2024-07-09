7333ad19197b9a61a30c8f4abc589d40af0af0d6
SahanWeerasiri <131955636+SahanWeerasiri@users.noreply.github.com>
2024-07-09 20:08:34 +0530

    Change the code

--- sort_numbers.py
+++ sort_numbers.py
@@ -2,9 +2,10 @@ import json
 import random
 
 def generate_and_sort_numbers(count=1):
-    numbers = [random.randint(1, 1000) for _ in range(count)]
-    sorted_numbers = sorted(numbers)
-    return numbers, sorted_numbers
+    #numbers = [random.randint(1, 1000) for _ in range(count)]
+    #sorted_numbers = sorted(numbers)
+    #return numbers, sorted_numbers
+    return [[5]],[[5*3]]
 
 def main():
     original, sorted_list = generate_and_sort_numbers()
